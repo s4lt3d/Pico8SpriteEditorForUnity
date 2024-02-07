3c57ed5d7e40fe5e4978ca4c048eb66e012ff75a
Walter Gordy <walter.gordy@gmail.com>
2024-02-07 11:21:50 -0700

    Refactoring and organization

--- Assets/Pico8/Editor/Pico8SpriteEditor.cs
+++ Assets/Pico8/Editor/Pico8SpriteEditor.cs
@@ -5,9 +5,9 @@ using System.Data;
 
 public class Pico8SpriteEditor : EditorWindow
 {
-    private Texture2D textureToPreview;
+    private Texture2D spriteSheetTexture;
     private Texture2D palletTexture;
-    private Texture2D textureSizeTexture;
+    private Texture2D zoomIconTexture;
     private Texture2D brushTexture;
     private Texture2D stampTexture;
     private Texture2D selectTexture;
@@ -15,12 +15,11 @@ public class Pico8SpriteEditor : EditorWindow
     private Texture2D paintTexture;
     private Texture2D circleTexture;
 
-
     private Rect textureRect;
     private Rect palletRect;
     private Rect spriteRect;
-    private Rect smallTextureRect; 
-    private Rect spriteNumberRect; 
+    private Rect smallTextureRect;
+    private Rect spriteNumberRect;
     private Rect zoomTextureRect;
     private Rect zoomSliderRect;
     private Rect spriteSheetRect;
@@ -51,39 +50,33 @@ public class Pico8SpriteEditor : EditorWindow
         var window = GetWindow<Pico8SpriteEditor>();
         window.titleContent = new GUIContent("Pico-8 Sprite Editor");
 
-        Vector2 windowSize = new Vector2(512 + 60, 256+512+30+30+30); // Example size, adjust as needed
+        Vector2 windowSize = new Vector2(512 + 60, 256 + 512 + 30 + 30 + 30); // Example size, adjust as needed
         window.minSize = windowSize;
         window.maxSize = windowSize;
 
         window.Show();
     }
 
-    void LoadTexture()
+    void Initialize()
     {
-        textureToPreview = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/test.png");
+        spriteSheetTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/test.png");
         palletTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Pico8/pallet.png");
-        textureSizeTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Pico8/texturesize.png");
-        brushTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Pico8/brush.png"); ;
-        stampTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Pico8/stamp.png"); ;
-        selectTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Pico8/select.png"); ;
-        handTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Pico8/hand.png"); ;
-        paintTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Pico8/paint.png"); ;
-        circleTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Pico8/circle.png"); ;
+        zoomIconTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Pico8/texturesize.png");
+        brushTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Pico8/brush.png");
+        stampTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Pico8/stamp.png");
+        selectTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Pico8/select.png");
+        handTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Pico8/hand.png");
+        paintTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Pico8/paint.png");
+        circleTexture = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/Pico8/circle.png");
 
         colorsPerRow = palletTexture.width;
-    }
 
-    private void OnEnable()
-    {
-        LoadTexture();
         textureRect = new Rect(30, 15, 256, 256);
-        
-        
+
         palletRect = new Rect(256 + 30 + 15, 15, 128, 128);
-        zoomSliderRect = new Rect(256 + 30 + 15 + 40 , 128 + 30 + 30, 140, 20);
+        zoomSliderRect = new Rect(256 + 30 + 15 + 40, 128 + 30 + 30, 140, 20);
         zoomTextureRect = new Rect(256 + 30 + 15, 128 + 30 + 24, 32, 32);
         spriteSheetRect = new Rect(30, 256 + 30 + 30, 512, 512);
-
 
         smallTextureRect = new Rect(256 + 20, 256 + 21, 32, 32);
         spriteNumberRect = new Rect(256 + 20 + 32, 256 + 22, 64, 36);
@@ -93,10 +86,14 @@ public class Pico8SpriteEditor : EditorWindow
         handIconRect = new Rect(30 + 40 * 3, 256 + 21, 32, 32);
         paintIconRect = new Rect(30 + 40 * 4, 256 + 21, 32, 32);
         circleIconRect = new Rect(30 + 40 * 5, 256 + 21, 32, 32);
+    }
 
-}
+    private void OnEnable()
+    {
+        Initialize();
+    }
 
-    private void DrawThickRectangle(Rect rectangle, float thickness, Color color)
+    private void DrawRectangle(Rect rectangle, float thickness, Color color)
     {
         Handles.BeginGUI();
         Handles.color = color;
@@ -128,97 +125,85 @@ public class Pico8SpriteEditor : EditorWindow
 
     private void OnGUI()
     {
+        HandleUserInput();
+
+        DrawSpriteEditView();
+
+        DrawTools();
+
+        DrawPallet();
+
+        DrawZoomSlider();
+
+        DrawSpriteLargeSheet();
+    }
+
+    private void DrawSpriteLargeSheet()
+    {
+        DrawTexture(spriteSheetTexture, spriteSheetRect, false);
+
+
+        DrawSpriteSelection();
+    }
+
+    private void DrawZoomSlider()
+    {
         textureZoom = EditorGUI.IntSlider(zoomSliderRect, "", textureZoom, 0, 3);
 
         zoomFactor = 8 * (int)Mathf.Pow(2, textureZoom);
 
-        HandleMouseClicks();
+        DrawTexture(zoomIconTexture, zoomTextureRect);
 
-        DrawThickRectangle(textureRect, 1, Color.black);
+        
+    }
 
-        if (textureToPreview != null)
-        {
-            Rect spriteRect = GridConversion.ToRect(spriteSelection, spritesPerRow, zoomFactor, zoomFactor);
-            spriteRect.x *= 8;
-            spriteRect.y *= 8;
-            DrawTexture(textureToPreview, textureRect, spriteRect);
-            DrawTexture(textureToPreview, smallTextureRect, spriteRect);
-            DrawTexture(textureToPreview, spriteSheetRect);
-            DrawTexture(brushTexture, brushIconRect);
-            DrawTexture(stampTexture, stampIconRect);
-            DrawTexture(selectTexture, selectIconRect);
-            DrawTexture(handTexture, handIconRect);
-            DrawTexture(paintTexture, paintIconRect);
-            DrawTexture(circleTexture, circleIconRect);
-
-            GUIStyle labelStyle = new GUIStyle(GUI.skin.label);
-            labelStyle.fontSize = 24; // Example: Change font size
-            labelStyle.normal.textColor = Color.gray; // Example: Change text color
-            labelStyle.fontStyle = FontStyle.Bold;
-            labelStyle.alignment = TextAnchor.MiddleLeft; // Example: Center the text
-
-            // Draw the label with the custom style
-            GUI.Label(spriteNumberRect, spriteSelection.ToString("000"), labelStyle);
-            
-        }
-
-        if (palletTexture != null)
-        {
-            DrawTexture(palletTexture, palletRect);
-        }
-
-        if (textureSizeTexture != null)
-        {
-            DrawTexture(textureSizeTexture, zoomTextureRect);
-        }
-
+    private void DrawPallet()
+    {
+        DrawTexture(palletTexture, palletRect);
         DrawPalletSelection();
-        DrawSpriteSelection();
     }
 
-    private void DrawSpriteSelection()
+    private void DrawTools()
     {
-        float spriteSelectionWidth =  spriteSheetRect.width / spritesPerRow;
-        float spriteSelectionHeight =  spriteSheetRect.height / spritesPerRow; 
+        DrawTexture(brushTexture, brushIconRect, false);
+        DrawTexture(stampTexture, stampIconRect, true);
+        DrawTexture(selectTexture, selectIconRect, true);
+        DrawTexture(handTexture, handIconRect, true);
+        DrawTexture(paintTexture, paintIconRect, true);
+        DrawTexture(circleTexture, circleIconRect, true);
 
-        Vector2 gridPosition = GridConversion.ToVector(spriteSelection, spritesPerRow);
 
-        Rect sheetPosition = new Rect(
-            spriteSheetRect.x + gridPosition.x * spriteSelectionWidth,
-            spriteSheetRect.y + gridPosition.y * spriteSelectionHeight,
-            spriteSelectionWidth * (int)Mathf.Pow(2, textureZoom), spriteSelectionHeight * (int)Mathf.Pow(2, textureZoom));
-
-        DrawThickRectangle(sheetPosition, selectionRectThickness, Color.white);
+        // draw tiny preview 
+        DrawTexture(spriteSheetTexture, smallTextureRect, false, spriteRect);
+        // Draw sprite number next to tiny preview
+        GUIStyle labelStyle = new GUIStyle(GUI.skin.label);
+        labelStyle.fontSize = 24;
+        labelStyle.normal.textColor = Color.gray;
+        labelStyle.fontStyle = FontStyle.Bold;
+        labelStyle.alignment = TextAnchor.MiddleLeft;
+        GUI.Label(spriteNumberRect, spriteSelection.ToString("000"), labelStyle);
     }
 
-    private void DrawPalletSelection()
+    private void DrawSpriteEditView()
     {
-        float palletSelectionWidth = palletRect.width / colorsPerRow; 
-        float palletSelectionHeight = palletRect.height / palletTexture.height;
-        Vector2 gridPosition = GridConversion.ToVector(palletSelection, colorsPerRow);
-
-        Rect palletPosition = new Rect(
-            palletRect.x + gridPosition.x * palletSelectionWidth,
-            palletRect.y + gridPosition.y * palletSelectionHeight,
-            palletSelectionWidth, palletSelectionHeight);
-
-        DrawThickRectangle(palletPosition, selectionRectThickness, Color.white);
+        // draw sprite in main window
+        DrawTexture(spriteSheetTexture, textureRect, false, spriteRect);
+        
+        // draw outline for sprite
+        DrawRectangle(textureRect, 1, Color.black);
     }
 
-
-    private void SetPixel(int x, int y, Color c)
+    private void HandleUserInput()
     {
-        textureToPreview.SetPixel(x, y, c);
-        textureToPreview.Apply(); 
-    }
+        // update spriteRect for which sprites are selected
+        spriteRect = GridConversion.ToRect(spriteSelection, spritesPerRow, zoomFactor, zoomFactor);
+        spriteRect.x *= 8;
+        spriteRect.y *= 8;
 
-    
-
-    private void HandleMouseClicks()
-    {
         Event e = Event.current;
-        if ((e.type == EventType.MouseDown || e.type == EventType.MouseDrag) && e.button == 0) 
+        if ((e.type == EventType.MouseDown || e.type == EventType.MouseDrag) && e.button == 0)
         {
+
             Vector2 mousePos = e.mousePosition;
             if (textureRect.Contains(mousePos))
             {
@@ -237,8 +222,10 @@ public class Pico8SpriteEditor : EditorWindow
                 originalY += (int)spriteRect.y;
 
                 shouldSave = true;
-                SetPixel(originalX, textureToPreview.height - originalY - 1, palletColor);
+                SetPixel(spriteSheetTexture, originalX, spriteSheetTexture.height - originalY - 1, palletColor);
                 Repaint();
+
+
 
             }
             else if (palletRect.Contains(mousePos))
@@ -252,7 +239,8 @@ public class Pico8SpriteEditor : EditorWindow
                     palletColor = new Color(0, 0, 0, 0);
                 Repaint();
             }
-            else if(spriteSheetRect.Contains(mousePos)) {
+            else if (spriteSheetRect.Contains(mousePos))
+            {
                 mousePos.x -= spriteSheetRect.x;
                 mousePos.y -= spriteSheetRect.y;
                 mousePos /= 32f;
@@ -262,10 +250,11 @@ public class Pico8SpriteEditor : EditorWindow
             }
         }
 
-        if((e.type == EventType.MouseLeaveWindow || e.type == EventType.MouseUp))
+        if ((e.type == EventType.MouseLeaveWindow || e.type == EventType.MouseUp))
         {
-            if(shouldSave) {
-                byte[] bytes = textureToPreview.EncodeToPNG();
+            if (shouldSave)
+            {
+                byte[] bytes = spriteSheetTexture.EncodeToPNG();
                 var path = "Assets/test.png";
                 if (path.Length != 0)
                 {
@@ -276,7 +265,42 @@ public class Pico8SpriteEditor : EditorWindow
         }
     }
 
-    private void DrawTexture(Texture2D texture, Rect position, Rect sourceRect = new Rect())
+    private void SetPixel(Texture2D texture, int x, int y, Color c)
+    {
+        texture.SetPixel(x, y, c);
+        texture.Apply();
+    }
+
+    private void DrawSpriteSelection()
+    {
+        float spriteSelectionWidth = spriteSheetRect.width / spritesPerRow;
+        float spriteSelectionHeight = spriteSheetRect.height / spritesPerRow;
+
+        Vector2 gridPosition = GridConversion.ToVector(spriteSelection, spritesPerRow);
+
+        Rect sheetPosition = new Rect(
+            spriteSheetRect.x + gridPosition.x * spriteSelectionWidth,
+            spriteSheetRect.y + gridPosition.y * spriteSelectionHeight,
+            spriteSelectionWidth * (int)Mathf.Pow(2, textureZoom), spriteSelectionHeight * (int)Mathf.Pow(2, textureZoom));
+
+        DrawRectangle(sheetPosition, selectionRectThickness, Color.white);
+    }
+
+    private void DrawPalletSelection()
+    {
+        float palletSelectionWidth = palletRect.width / colorsPerRow;
+        float palletSelectionHeight = palletRect.height / palletTexture.height;
+        Vector2 gridPosition = GridConversion.ToVector(palletSelection, colorsPerRow);
+
+        Rect palletPosition = new Rect(
+            palletRect.x + gridPosition.x * palletSelectionWidth,
+            palletRect.y + gridPosition.y * palletSelectionHeight,
+            palletSelectionWidth, palletSelectionHeight);
+
+        DrawRectangle(palletPosition, selectionRectThickness, Color.white);
+    }
+
+    private void DrawTexture(Texture2D texture, Rect position, bool dim = false, Rect sourceRect = new Rect())
     {
         if (sourceRect.width == 0)
         {
@@ -285,18 +309,26 @@ public class Pico8SpriteEditor : EditorWindow
         }
         else
         {
-            // Invert the Y coordinate for the sourceRect to align with GUI's top-left origin
             float yInverted = texture.height - sourceRect.y - sourceRect.height;
 
-            // Convert sourceRect from pixel coordinates to normalized coordinates, with Y-axis inverted
             float xMin = sourceRect.x / texture.width;
             float xMax = (sourceRect.x + sourceRect.width) / texture.width;
-            // Use yInverted for the Y coordinate calculations
+
             float yMin = yInverted / texture.height;
             float yMax = (yInverted + sourceRect.height) / texture.height;
 
             Rect texCoords = new Rect(xMin, yMin, xMax - xMin, yMax - yMin);
             GUI.DrawTextureWithTexCoords(position, texture, texCoords, true);
+        }
+
+        if (dim)
+        {
+
+            Color dimColor = new Color(0, 0, 0, 0.5f);
+            Color previousColor = GUI.color;
+            GUI.color = dimColor;
+            GUI.DrawTexture(position, EditorGUIUtility.whiteTexture);
+            GUI.color = previousColor;
         }
     }
 }
@@ -312,11 +344,11 @@ public class GridConversion
     {
         Vector2 grid = Vector2.zero;
         grid.x = index % gridWidth;
-        grid.y = index / gridWidth; 
+        grid.y = index / gridWidth;
         return grid;
     }
 
-    public static Rect ToRect(int index, int gridWidth, int width=0, int height=0)
+    public static Rect ToRect(int index, int gridWidth, int width = 0, int height = 0)
     {
         Vector2 v = ToVector(index, gridWidth);
         Rect r = new Rect(v.x, v.y, width, height);
